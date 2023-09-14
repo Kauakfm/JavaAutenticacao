85d6ff941f75698fe5eddff74b9e349999336a28
kauamartinsvar <kaua.martins@varsolutions.com.br>
2023-09-14 09:23:21 -0300

    Autenticao feita e funcionando

---
+++ var.solutions/src/main/java/com/br/var/solutions/security/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.br.var.solutions.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Autowired
+    private JwtAuthenticationEntryPoint JwtAuthenticationEntryPoint;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter requestFilter;
+
+protected void configure(HttpSecurity httpSecurity) throws Exception
+{
+    httpSecurity.csrf().disable()
+            .authorizeRequests().antMatchers("/pessoa/authorization", "/pessoa/authenticate", "/configuration/**", "/webjars/**").permitAll()
+            .anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(JwtAuthenticationEntryPoint).and().sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            httpSecurity.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+
+}
+}
